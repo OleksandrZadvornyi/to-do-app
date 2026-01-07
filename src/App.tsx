b2aa78323076ee771efbfd7a774fd212ad9f2e83
Oleksandr Zadvornyi <zadvornyi.alex16@gmail.com>
2026-01-07 16:27:33 +0200

    refactor: extract todo list item into TodoItem component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { TodoItem } from './components/TodoItem';
 
 type TodoFormProps = {
   onAdd: (text: string) => void;
@@ -80,39 +81,12 @@ export default function App() {
         {/* Render the list */}
         <ul className="space-y-3">
           {todos.map(todo => (
-            <li
+            <TodoItem
               key={todo.id}
-              className={`flex items-center p-3 rounded-lg border transition-all ${todo.completed
-                ? 'bg-gray-50 border-gray-200'
-                : 'bg-white border-gray-200 hover:border-blue-300 hover:shadow-sm'
-                }`}
-            >
-              {/* Checkbox triggers the toggle logic */}
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => toggleTodo(todo.id)}
-                className="h-5 w-5 text-blue-600 rounded border-gray-300 focus:ring-blue-500 cursor-pointer mr-4"
-              />
-
-              <span
-                className={`grow text-lg ${todo.completed ? 'line-through text-gray-400' : 'text-gray-700'
-                  }`}
-              >
-                {todo.text}
-              </span>
-
-              {/* Delete button triggers delete logic */}
-              <button
-                onClick={() => deleteTodo(todo.id)}
-                className="text-gray-400 hover:text-red-500 p-2 rounded-full hover:bg-red-50 transition-colors"
-                title="Delete task"
-              >
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-                </svg>
-              </button>
-            </li>
+              todo={todo}
+              onToggle={toggleTodo}
+              onDelete={deleteTodo}
+            />
           ))}
         </ul>
 
